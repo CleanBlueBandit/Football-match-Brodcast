c25737853f3e5227471c8ddd6bcc0cd7502f2f8d
CleanBlueBandit <122689110+CleanBlueBandit@users.noreply.github.com>
2026-09-08 17:52:25 +0400

    Rotate database password in db.php
    Updated database password to a new value for security.

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 // db.php
-$host = 'sql105.infinityfree.com'; // Replace with your InfinityFree MySQL Hostname
-$db_user = 'if0_41972243'; // Replace with your InfinityFree MySQL Username
-$db_pass = 'ooZHEKr9rJj'; // Replace with your vPanel password
-$db_name = 'if0_41972243_pswd_db'; // Replace with your Database Name
+$host = 'sql105.infinityfree.com'; 
+$db_user = 'if0_41972243';
+$db_pass = 'whoops, i disclosed this in the previous version. no worries tho, its rotated';
+$db_name = 'if0_41972243_pswd_db';
 
 // Create connection using MySQLi
 $conn = new mysqli($host, $db_user, $db_pass, $db_name);
